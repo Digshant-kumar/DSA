0030597c938fcd69be1672a24797008f7390164f
Digshant-kumar <62698559+Digshant-kumar@users.noreply.github.com>
2022-01-11 17:00:50 +0530

    Add files via upload

---
+++ pattern4.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number of lines: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j>0;j--)
+        {
+            cout<<" ";
+        }
+        for(int j=n-i;j>0;j--)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
